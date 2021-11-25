5e2d272477763e55e47965c7a7c87bea93d4b845
downwheel <downwheel@naver.com>
2021-11-25 09:46:00 +0900

    21.11.25 0945 commit

--- src/main/java/com/bbs/controller/MainController.java
+++ src/main/java/com/bbs/controller/MainController.java
@@ -16,25 +16,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.bbs.service.UsersService;
 
 @Controller
-public class HomeController {
+public class MainController {
 	
-	@Inject
-	UsersService service;
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "main/main";
 	}
 	
 }
--- src/main/java/com/bbs/dao/UsersDAOImpl.java
+++ src/main/java/com/bbs/dao/UsersDAOImpl.java
@@ -1,5 +1,8 @@
 package com.bbs.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UsersDAOImpl implements UsersDAO {
 
 	@Override
